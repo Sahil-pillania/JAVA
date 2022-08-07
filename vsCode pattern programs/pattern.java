e4f0048e71432bcbd0bb77aa59b97ff1e07c7f0e
Sahil pillania <sahilpillania88@gmail.com>
2022-08-07 22:20:50 +0530

    program

---
+++ vsCode pattern programs/pattern.java
@@ -0,0 +1,15 @@
+
+
+public static void main(String arg[]){
+    int n = 4;
+    int m = 5;
+
+    // outer loop 
+    for(int i=1; i<=n; i++){
+        // inner loop 
+        for(int j=1; j<=m; j++){
+            System.out.print('*');
+        }
+        System.out.print('\n');
+    }
+}
