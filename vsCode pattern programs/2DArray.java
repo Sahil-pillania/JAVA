a1d2abd75e3880eaefc2e8a06351247fa0136859
Sahil pillania <sahilpillania88@gmail.com>
2022-08-09 21:27:29 +0530

    TwoDArray

---
+++ vsCode pattern programs/2DArray.java
@@ -0,0 +1,26 @@
+public class 2DArray {
+    public static  void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+
+        int[][] numbers = new int[rows][cols];
+
+        // inputting the data 
+        for(int i=0; i<rows; i++){
+
+            for(int j=0; j<cols; j++){
+                
+                numbers[i][j] = sc.nextInt();
+            }
+        }
+
+        // output of data 
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<cols; j++){
+                System.out.print(numbers[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
